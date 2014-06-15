5ddb8b6fd810798205084c59b885b757826671a7
Li He <jet.in.brain@gmail.com>
2014-06-15 23:54:46 +0800

    finish Manager Part

--- src/main/java/biz/guanggu/Manager.java
+++ src/main/java/biz/guanggu/Manager.java
@@ -1,5 +1,6 @@
 package biz.guanggu;
 
+import javafx.util.Pair;
 import org.apache.http.Header;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpPost;
@@ -24,10 +25,31 @@ public class Manager {
 
     public static void main(String[] args) {
         ErrorLogger logger = new ErrorLogger();
-        Manager.stop(logger);
+
+        execute("in", "start", logger);
     }
 
-    public static String handleResult(InputStream inputStream) throws IOException {
+    public static boolean execute(String path,String action,ErrorLogger logger){
+        try{
+            Pair<String, String> key = login();
+
+            String sessionId = key.getKey();
+            String csrf = key.getValue();
+
+            URL url = new URL(base+"/manager/html/"+action+"?path=/"+path+"&org.apache.catalina.filters.CSRF_NONCE="+csrf);
+
+            connect(url,sessionId);
+
+            return true;
+        }catch (Exception exception){
+            logger.log(exception.getMessage()+"\n"+ Arrays.toString(exception.getStackTrace()));
+            exception.printStackTrace();
+            return false;
+        }
+    }
+
+
+    public static String handleResult(InputStream inputStream, boolean display) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
 
         String line;
@@ -35,7 +57,8 @@ public class Manager {
 
         while ((line = reader.readLine()) != null) {
             builder.append(line);
-            //System.out.println(line);
+            if(display)
+                System.out.println(line);
         }
         reader.close();
 
@@ -54,143 +77,48 @@ public class Manager {
         return csrf;
     }
 
-    public static boolean stop(ErrorLogger logger) {
+    public static void connect(URL url, String sessionId) throws IOException, URISyntaxException {
+        CloseableHttpClient httpClient = HttpClients.createMinimal();
 
-        try{
-            URL url = new URL(base+"/manager/html");
+        HttpPost post = new HttpPost(url.toURI());
+        post.setHeader("Authorization","Basic YWRtaW46MTE2MDUy");
+        post.setHeader("Cache-Control","max-age=0");
+        post.setHeader("Cookie","JSESSIONID="+sessionId);
 
-            Authenticator.setDefault(new MyAuth());
-            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        CloseableHttpResponse response = httpClient.execute(post);
+//        for(Header header : response.getAllHeaders()){
+//            System.out.println(header.toString());
+//        }
 
-            String cookieHeader = connection.getHeaderField("Set-Cookie");
-            String sessionId = cookieHeader.substring(cookieHeader.indexOf("=")+1, cookieHeader.indexOf(";"));
+//        handleResult(response.getEntity().getContent(), false);
 
-            String csrf = handleResult(connection.getInputStream());
-
-            connection.disconnect();
-
-            //System.out.println(csrf);
-            //System.out.println(sessionId);
-
-            URL stopInURL = new URL(base+"/manager/html/stop?path=/in&org.apache.catalina.filters.CSRF_NONCE="+csrf);
-            URL stopOutURL = new URL(base+"/manager/html/stop?path=/out&org.apache.catalina.filters.CSRF_NONCE="+csrf);
-
-            CloseableHttpClient httpClient = HttpClients.createDefault();
-
-            HttpPost post = new HttpPost(stopInURL.toURI());
-            post.setHeader("Authorization","Basic YWRtaW46MTE2MDUy");
-            post.setHeader("Cache-Control","max-age=0");
-            post.setHeader("Content-Length","0");
-            post.setHeader("Cookie","JSESSIONID=D60CF8B2CE1B9CC51E51CE8A04284E3C");
-
-            CloseableHttpResponse response = httpClient.execute(post);
-            for(Header header : response.getAllHeaders()){
-                System.out.println(header.toString());
-            }
-
-            HttpURLConnection stopInConnection = (HttpURLConnection) stopInURL.openConnection();
-            stopInConnection.setRequestProperty("Cookie","JSESSIONID="+sessionId);
-            stopInConnection.setRequestMethod("POST");
-            stopInConnection.setUseCaches(false);
-            stopInConnection.setDoOutput(true);
-            stopInConnection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
-
-            stopInConnection.connect();
-
-            DataOutputStream out = new DataOutputStream(connection.getOutputStream());
-            String content = "submit=" + URLEncoder.encode("stop", "utf-8");
-            out.writeBytes(content);
-            out.flush();
-            out.close(); // flush and close
-
-            reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-            while ((line = reader.readLine()) != null) {
-                System.out.println(line);
-            }
-            reader.close();
-
-
-            stopInConnection.disconnect();
-
-            HttpURLConnection stopOutConnection = (HttpURLConnection) stopOutURL.openConnection();
-            stopOutConnection.setRequestProperty("Cookie","JSESSIONID="+sessionId);
-            stopOutConnection.setRequestMethod("POST");
-            stopOutConnection.setUseCaches(false);
-            stopOutConnection.setDoOutput(true);
-            stopOutConnection.setRequestProperty("Content-Type","application/x-www-form-urlencoded");
-
-            stopOutConnection.connect();
-            stopOutConnection.disconnect();
-
-            return true;
-        }catch (Exception exception){
-            logger.log(exception.getMessage()+"\n"+ Arrays.toString(exception.getStackTrace()));
-            return false;
-        }
+        response.close();
+        httpClient.close();
 
     }
 
+    /**
+     * login get session and csrf
+     * @return pair
+     * @throws IOException
+     */
+    public static Pair<String, String> login() throws IOException {
+        URL url = new URL(base+"/manager/html");
 
-    public static boolean start(ErrorLogger logger) {
+        Authenticator.setDefault(new MyAuth());
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
 
-        try{
-            URL url = new URL(base+"/manager/html");
+        String cookieHeader = connection.getHeaderField("Set-Cookie");
 
-            Authenticator.setDefault(new MyAuth());
-            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        String sessionId = cookieHeader.substring(cookieHeader.indexOf("=")+1, cookieHeader.indexOf(";"));
+        String csrf = handleResult(connection.getInputStream(), false);
 
-            String cookieHeader = connection.getHeaderField("Set-Cookie");
-            String sessionId = cookieHeader.substring(cookieHeader.indexOf("=")+1, cookieHeader.indexOf(";"));
+        connection.disconnect();
 
-            BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-
-            String line;
-            StringBuilder builder = new StringBuilder();
-
-            while ((line = reader.readLine()) != null) {
-                builder.append(line);
-                //System.out.println(line);
-            }
-            reader.close();
-            connection.disconnect();
-
-            String result = builder.toString();
-
-            Pattern pattern = Pattern.compile("/manager/html/stop(;jsessionid=[A-Z0-9])?\\?path=/&amp;org\\.apache\\.catalina\\.filters\\.CSRF_NONCE=([A-Z0-9]+)");
-
-            Matcher matcher = pattern.matcher(result);
-
-            String csrf = null;
-            if(matcher.find()){
-                csrf = matcher.group(2);
-            }
-
-            URL stopInURL = new URL(base+"/manager/html/start;jsessionid="+sessionId+"?path=/in&org.apache.catalina.filters.CSRF_NONCE="+csrf);
-            URL stopOutURL = new URL(base+"/manager/html/start;jsessionid="+sessionId+"?path=/out&org.apache.catalina.filters.CSRF_NONCE="+csrf);
-
-            HttpURLConnection startInConnection = (HttpURLConnection) stopInURL.openConnection();
-            startInConnection.setRequestProperty("Cookie", "JSESSIONID=" + sessionId);
-            startInConnection.setRequestMethod("POST");
-            startInConnection.setUseCaches(false);
-            startInConnection.connect();
-
-            startInConnection.disconnect();
-
-            HttpURLConnection startOutConnection = (HttpURLConnection) stopOutURL.openConnection();
-            startOutConnection.setRequestProperty("Cookie","JSESSIONID="+sessionId);
-            startOutConnection.setRequestMethod("POST");
-            startOutConnection.setUseCaches(false);
-            startOutConnection.connect();
-            startOutConnection.disconnect();
-
-            return true;
-        }catch (Exception exception){
-            logger.log(exception.getMessage()+"\n"+ Arrays.toString(exception.getStackTrace()));
-            return false;
-        }
-
-
+       return new Pair<String, String>(sessionId,csrf);
     }
+
+
 
     private static class MyAuth extends Authenticator {
 
